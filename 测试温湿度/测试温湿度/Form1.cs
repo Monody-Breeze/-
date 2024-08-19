5c51c51580642d6c79bec25522aec35daa7c614d
zccc <3492912044@qq.com>
2024-08-19 21:26:54 +0800

    代码

--- 测试温湿度/测试温湿度/Form1.cs
+++ 测试温湿度/测试温湿度/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace 测试温湿度
 {
@@ -15,6 +16,20 @@ namespace 测试温湿度
         public Form1()
         {
             InitializeComponent();
+            Title time = new Title("时间", Docking.Bottom);
+            Title wsy = new Title("温度/湿度/氧气", Docking.Left);
+            Title fs = new Title("风速", Docking.Right);
+            chart1.Titles.Add(time);
+            chart1.Titles.Add(wsy);
+            chart1.Titles.Add(fs);
+            chart1.Series.Add("温度");
+            chart1.Series.Add("湿度");
+            chart1.Series.Add("氧气");
+            chart1.Series.Add("风速");
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+            chart1.Series[1].ChartType = SeriesChartType.Line;
+            chart1.Series[2].ChartType = SeriesChartType.Line;
+            chart1.Series[3].ChartType = SeriesChartType.Line;
         }
     }
 }
